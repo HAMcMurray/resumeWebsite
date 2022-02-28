b9cebda7d975625225dad5a4deb3214ebdf5c2fc
HAMcMurray <79503099+HAMcMurray@users.noreply.github.com>
2022-02-28 00:02:19 -0600

    dont remember

--- resumeSite/src/app/home/home.component.ts
+++ resumeSite/src/app/home/home.component.ts
@@ -15,10 +15,14 @@ import { Component, OnInit } from '@angular/core';
           <div id="info">
           <h1>Skills</h1>
           <ul>
-              <li>* problem solving, coordination, leadership, and team collaboration. </li>
-              <li>* Java, Python, C, SQL & Front-End Web Design</li>
-              <li>* Adobe Photoshop, Audition, Premiere, & After Effects</li>
-              <li>* Davinci Resolve & RX6</li>
+              <li>Software Engineering</li>
+              <li></li>
+              <li>Web Design</li>
+              <li></li>
+              <li>Motion Graphics</li>
+              <li></li>
+              <li>Graphic Design</li>
+              <li></li>
           </ul>
           <button><a routerLink="/videos">Videos</a></button><button><a routerLink="/photos">Photos</a></button>
           </div>
@@ -35,15 +39,23 @@ import { Component, OnInit } from '@angular/core';
           <div>
               <h1>Experience</h1>
               <ul>
-                  <li>BS Information Technology from University of Missouri</li>
-                  <li>TA for Multiple Media Design courses at Mizzou</li>
-                  <li>2nd Assistant Director for a developing TV pilot</li>
-                  <li>Team Memeber and Head Trainer at Target</li>
+                  <li>BS Information Technology</li>
+                  <li></li>
+                  <li>Sales Accociate</li>
+                  <li></li>
+                  <li>Teaching Assistant</li>
+                  <li></li>
+                  <li>Media Design Checkout</li>
+                  <li></li>
+                  <li>Interactive Digital Lab Assistant</li>
+                  <li></li>
+                  <li>2nd Assistant Director</li>
               </ul>
           <button><a routerLink="/about">More About Me</a></button>
           </div>
       </mat-card-content>
   </mat-card>
+  <p></p>
 </div>
 `,
   styleUrls: ['./home.component.css']
